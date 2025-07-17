373b8d5fe1f6de0e7a79aa2173c427cdf71bc698
Emre <yun.emrekaya138@gmail.com>
2025-07-17 11:09:09 +0300

    error message for negativ numbers

--- app.py
+++ app.py
@@ -15,11 +15,20 @@ def is_prime(n):
 @app.route('/', methods=['GET', 'POST'])
 def index():
     primes = []
-    number = "X"
+    number = "n"
 
+    #This line allows Flask to execute the following commands if an input is submitted through the HTML
     if request.method == 'POST':
-        number = int(request.form['number'])
-        primes = [num for num in range(1, number + 1) if is_prime(num)]
+        if int(request.form['number']) <= 0:
+            warning = "Please enter a positive number."
+            
+            return render_template('index.html', warning=warning)
+        
+        else:
+            number = int(request.form['number'])
+            primes = [num for num in range(1, number + 1) if is_prime(num)]
+            #his variable will use for data base section of the project
+            primeCount = len(primes)          
 
     return render_template('index.html', primes=primes, number=number)
 
